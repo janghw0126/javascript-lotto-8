04eaa43e950ab7695bd09af3ba8cbc63ce8f7b17
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-11-03 17:49:57 +0900

    refactor(App): 로또 구입 금액 예외 처리 수정 및 추가

--- src/App.js
+++ src/App.js
@@ -25,12 +25,15 @@ class App {
   }
 
   validatePurchaseAmount(input){
-    if(isNaN(input))  throw new Error("[ERROR] 숫자가 아닌 문자를 입력 받았습니다.");
-    if(input%1000 !== 0) throw new Error("[ERROR] 1000원 단위로 입력 받지 않았습니다.");
-    if(input<=0)  throw new Error("[ERROR] 0 이하의 금액을 입력받았습니다.")
-    
-    return input;
+    if (input === "") throw new Error("[ERROR] 빈 값을 입력받았습니다.");
+    if (isNaN(input)) throw new Error("[ERROR] 숫자가 아닌 문자를 입력 받았습니다.");
+  
+    const amount = Number(input);
 
+    if (amount <= 0) throw new Error("[ERROR] 0 이하의 금액을 입력받았습니다.");
+    if (amount % 1000 !== 0) throw new Error("[ERROR] 1000원 단위로 입력 받지 않았습니다.");
+
+    return amount;
   }
 
   LottoGenerator(purchaseAmount){
