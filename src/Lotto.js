6256747b7ba5d7c37c2ceaed5cb2a76a6fb10480
장혜원 <127714800+janghw0126@users.noreply.github.com>
2025-11-03 16:42:31 +0900

    feat(Lotto): 로또 번호를 문자열 형태로 표현하는 기능 추가

--- src/Lotto.js
+++ src/Lotto.js
@@ -11,8 +11,11 @@ class Lotto {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
   }
-
-  // TODO: 추가 기능 구현
+  
+  // 로또 번호 배열을 문자열 형태로 표현
+  toString(){
+    return `[${this.numbers.join(", ")}]`;
+  }
 }
 
 export default Lotto;
